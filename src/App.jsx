5e41c877e28ebb7d85854d8a1bab42967de096e5
Shaun Parkison <shparkison@gmail.com>
2026-01-30 07:14:13 -0700

    chore: Update play icon

--- src/App.jsx
+++ src/App.jsx
@@ -165,8 +165,8 @@ function PlayerDemo() {
           ) : (
             <div className="player-placeholder">
               <div className="placeholder-content">
-                <svg width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                  <polygon points="5 3 19 12 5 21 5 3" />
+                <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                  <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
                 </svg>
                 <p>Enter a URL above to start playing</p>
               </div>
